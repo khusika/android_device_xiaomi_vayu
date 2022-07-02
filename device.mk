b1b622e58b4fd772f37e03cde65c2c4a0b2c4bdc
Khusika Dhamar Gusti <mail@khusika.com>
2022-07-02 13:18:41 +0000

    vayu: temporary exlude xiaomiparts
    Signed-off-by: Khusika Dhamar Gusti <mail@khusika.com>

--- device.mk
+++ device.mk
@@ -136,8 +136,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
 # DeviceSettings
-PRODUCT_PACKAGES += \
-    XiaomiParts
+# PRODUCT_PACKAGES += \
+#     XiaomiParts
 
 # Dex
 ifneq ($(TARGET_BUILD_VARIANT),eng)
